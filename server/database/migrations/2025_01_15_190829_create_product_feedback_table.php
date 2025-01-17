7fcb595ed36620554254d9396f1d9fd181178bfb
CHiepTRAN <107661128+HHiepz@users.noreply.github.com>
2025-01-18 00:48:29 +0700

    Fix drop database 'product_feedback' to 'product_feedbacks'

--- server/database/migrations/2025_01_15_190829_create_product_feedback_table.php
+++ server/database/migrations/2025_01_15_190829_create_product_feedback_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_feedback');
+        Schema::dropIfExists('product_feedbacks');
     }
 };
