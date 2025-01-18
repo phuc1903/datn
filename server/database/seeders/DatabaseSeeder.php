05694d33551f818d6272ec2ed389c7808c6f960f
CHiepTRAN <107661128+HHiepz@users.noreply.github.com>
2025-01-19 03:10:17 +0700

    Update fake data for 'Blog' & 'User address', 'User cart', 'User favorite'

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
+use App\Models\BlogProduct;
+use App\Models\User;
+use App\Models\UserAddress;
+use App\Models\UserFavorite;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /*
+        |--------------------
+        | Admin (mặc định)
+        |--------------------
+        |
+        | Tài khoản:
+        | - admin@gmail.com
+        | - admin
+        |
+        */
+        Admin::factory()->create([
+            'first_name' => 'Hữu Hiệp',
+            'last_name' => 'Trần',
+            'phone_number' => '(+84) 1234-5678',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+            'status' => 'active',
+            'sex' => 'male',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        /*
+        |--------------------
+        | User (mặc định)
+        |--------------------
+        |
+        | Tài khoản:
+        | - user@gmail.com
+        | - user
+        |
+        */
+        User::factory()->create([
+            'first_name' => 'Hữu Hiệp',
+            'last_name' => 'Trần',
+            'phone_number' => '(+84) 1234-5678',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('user'),
+            'status' => 'active',
+            'sex' => 'male',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        /*
+        |--------------------
+        | Tạo các seeders khác
+        |--------------------
+         */
         $this->call([
             AdminSeeder::class,
             UserSeeder::class,
@@ -26,11 +80,19 @@ class DatabaseSeeder extends Seeder
             ProductTagSeeder::class,
             ProductCategorySeeder::class,
 
-            VoucherSeeder::class,
             OrderSeeder::class,
+            VoucherSeeder::class,
+            UserVoucherSeeder::class,
             OrderVoucherSeeder::class,
 
-            WalletSeeder::class
+            WalletSeeder::class,
+            UserAddressSeeder::class,
+            UserFavoriteSeeder::class,
+            UserCartSeeder::class,
+
+            BlogSeeder::class,
+            BlogTagSeeder::class,
+            BlogProductSeeder::class,
         ]);
     }
 }
