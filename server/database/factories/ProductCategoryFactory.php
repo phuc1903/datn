c004766b571b484be91b10ad2c3b88ee83236b33
CHiepTRAN <107661128+HHiepz@users.noreply.github.com>
2025-01-19 05:11:53 +0700

    Fix fake data: Automatically add parent category when creating child product category

--- server/database/factories/ProductCategoryFactory.php
+++ server/database/factories/ProductCategoryFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -22,5 +23,24 @@ class ProductCategoryFactory extends Factory
             'product_id' => Product::inRandomOrder()->first()->id,
             'category_id' => Category::inRandomOrder()->first()->id
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (ProductCategory $productCategory) {
+            /*
+            * Kiểm tra nếu danh mục được tạo là danh mục con (category_id có parent_id khác 0),
+            * thì tự động thêm bản ghi cho danh mục cha tương ứng.
+            */
+            $category = Category::find($productCategory->category_id);
+
+            if ($category && $category->parent_id != 0) {
+                // Tạo bản ghi ProductCategory mới cho danh mục cha.
+                ProductCategory::factory()->create([
+                    'product_id' => $productCategory->product_id,
+                    'category_id' => $category->parent_id
+                ]);
+            }
+        });
     }
 }
