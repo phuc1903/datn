87a4319599a98572ef64de5dd71086e1fb88fa6e
CHiepTRAN <107661128+HHiepz@users.noreply.github.com>
2025-01-21 01:40:33 +0700

    Update HistoryMoneyFactory.php

--- server/database/factories/HistoryMoneyFactory.php
+++ server/database/factories/HistoryMoneyFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class HistoryMonneyFactory extends Factory
+class HistoryMoneyFactory extends Factory
 {
     /**
      * Define the model's default state.
